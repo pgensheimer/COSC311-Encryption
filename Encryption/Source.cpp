e4fee7b8bf27f3bfee7372920060e638912b7e04
Tom Krobatsch <tkrobatsch@gmail.com>
2017-02-22 20:14:12 -0500

    Upload

---
+++ Encryption/Source.cpp
@@ -0,0 +1,33 @@
+/*
+Empty shell so we can start planning on structure and who works on what
+*/
+
+#include <iostream>
+#include "Source.h"
+
+using namespace std;
+
+//possible functions needed
+
+//encrypt
+
+//decrypt
+
+//read encrypted message in from file
+
+//export encrypted message out to file
+
+//spit out message based on ascii table
+
+//try and figure out if message makes sense
+
+
+
+/* things to be done
+
+
+
+
+
+
+*/
